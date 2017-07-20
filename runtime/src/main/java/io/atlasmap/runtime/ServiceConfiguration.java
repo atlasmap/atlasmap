6269a37f59523f32af9e8d97a361a2d1bcbf489a
Roland Huß <roland@ro14nd.de>
2017-07-20 20:28:52 +0200

    fix(mavenclasspath): Reenabled workaround and added connectors to classpath

--- runtime/src/main/java/io/atlasmap/runtime/ServiceConfiguration.java
+++ runtime/src/main/java/io/atlasmap/runtime/ServiceConfiguration.java
@@ -1,6 +1,12 @@
 package io.atlasmap.runtime;
 
+import javax.ws.rs.*;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+
 import io.atlasmap.java.service.JavaService;
+import io.atlasmap.java.v2.MavenClasspathRequest;
+import io.atlasmap.java.v2.MavenClasspathResponse;
 import io.atlasmap.service.AtlasJsonProvider;
 import io.atlasmap.service.AtlasService;
 import org.springframework.context.annotation.Bean;
@@ -11,7 +17,7 @@ public class ServiceConfiguration {
 
     @Bean
     public JavaService javaService() {
-        return new JavaService();
+        return new JavaServiceEmptyClasspath();
     }
 
     @Bean
@@ -22,5 +28,36 @@ public class ServiceConfiguration {
     @Bean
     public AtlasJsonProvider atlasJsonProvider() {
         return new AtlasJsonProvider();
+    }
+
+    // =====================================================================
+
+    public  static class JavaServiceEmptyClasspath extends JavaService {
+        public JavaServiceEmptyClasspath() {
+            super();
+        }
+
+        /**
+         * Stub out mavenclasspath processing for now.
+         *
+         * @param request
+         * @return
+         * @throws Exception
+         */
+        @POST
+        @Consumes({MediaType.APPLICATION_JSON})
+        @Produces({MediaType.APPLICATION_JSON})
+        @Path("/mavenclasspath")
+        public Response generateClasspath(MavenClasspathRequest request) throws Exception {
+            MavenClasspathResponse response = new MavenClasspathResponse();
+            response.setExecutionTime(0L);
+            response.setClasspath("");
+            return Response.ok()
+                           .header("Access-Control-Allow-Origin", "*")
+                           .header("Access-Control-Allow-Headers", "Content-Type")
+                           .header("Access-Control-Allow-Methods", "GET,PUT,POST,PATCH,DELETE")
+                           .entity(response)
+                           .build();
+        }
     }
 }
