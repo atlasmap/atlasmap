a97618f27519ee07a29c86d185dc020c53670bce
Zoran Regvart <zregvart@apache.org>
2017-10-04 16:50:25 +0200

    fix: Read the InputStream into a String
    This is a quick fix for #26, given an InputStream in the incoming
    message body then read it into the String and pass that along to the
    mapping engine as the parsers (JSON, XML) expect body to be String
    based.
    
    Fixes #26

---
+++ src/test/java/org/apache/camel/component/atlasmap/AtlasEndpointTest.java
@@ -0,0 +1,46 @@
+package org.apache.camel.component.atlasmap;
+
+import java.util.List;
+
+import io.atlasmap.v2.AtlasMapping;
+import io.atlasmap.v2.DataSource;
+import io.atlasmap.v2.DataSourceType;
+
+import org.junit.Test;
+
+import static org.apache.camel.component.atlasmap.AtlasEndpoint.isSourceXmlOrJson;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+
+public class AtlasEndpointTest {
+
+    @Test
+    public void shouldDetectIfSourceIsXmlOrJson() {
+        final AtlasMapping mapping = new AtlasMapping();
+
+        assertFalse("With no datasources no XML or JSON sources should be found", isSourceXmlOrJson(mapping));
+
+        final List<DataSource> dataSources = mapping.getDataSource();
+        final DataSource dataSource = new DataSource();
+        dataSource.setDataSourceType(DataSourceType.SOURCE);
+        dataSources.add(dataSource);
+
+        assertFalse("Source datasource without uri should not be detected as XML or JSON", isSourceXmlOrJson(mapping));
+
+        dataSource.setUri("atlas:java:some.Type");
+        assertFalse("Java source datasource should not be detected as XML or JSON", isSourceXmlOrJson(mapping));
+
+        dataSource.setUri("atlas:json:SomeType");
+        assertTrue("JSON data source should be detected", isSourceXmlOrJson(mapping));
+
+        dataSource.setDataSourceType(DataSourceType.TARGET);
+        assertFalse("JSON data source target should not be detected as XML or JSON", isSourceXmlOrJson(mapping));
+
+        dataSource.setDataSourceType(DataSourceType.SOURCE);
+        dataSource.setUri("atlas:xml?complexType=ns:SomeElement");
+        assertTrue("XML data source should be detected", isSourceXmlOrJson(mapping));
+
+        dataSource.setDataSourceType(DataSourceType.TARGET);
+        assertFalse("XML data source target should not be detected as XML or JSON", isSourceXmlOrJson(mapping));
+    }
+}
