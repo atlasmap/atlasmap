11afc6c06ff3299f1203e014a02dac9fbdeb895a
Joey Li <joey.li@mediadriver.com>
2017-12-22 12:05:15 -0800

    chore: Increase code coverage on JSON FieldReader and FieldWriter
    Relates to: #202

--- runtime/modules/json/core/src/test/java/io/atlasmap/json/core/JsonFieldReaderTest.java
+++ runtime/modules/json/core/src/test/java/io/atlasmap/json/core/JsonFieldReaderTest.java
@@ -22,6 +22,7 @@ import static org.junit.Assert.assertThat;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
 
+import java.io.File;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 
@@ -34,6 +35,9 @@ import io.atlasmap.json.v2.AtlasJsonModelFactory;
 import io.atlasmap.json.v2.JsonField;
 import io.atlasmap.spi.AtlasInternalSession;
 import io.atlasmap.spi.AtlasInternalSession.Head;
+import io.atlasmap.v2.AuditStatus;
+import io.atlasmap.v2.Audits;
+import io.atlasmap.v2.FieldType;
 
 public class JsonFieldReaderTest {
 
@@ -748,4 +752,308 @@ public class JsonFieldReaderTest {
         field.setValue(null);
         field.setFieldType(null);
     }
+
+    private AtlasInternalSession testBoundaryValue(String fileName, String fieldPath, FieldType fieldType, Object expectedObject) throws Exception {
+        String filePath = "src" + File.separator + "test" + File.separator + "resources"
+                + File.separator + "jsonFields" + File.separator + fileName;
+        String document = new String(Files.readAllBytes(Paths.get(filePath)));
+        reader.setDocument(document);
+        JsonField field = AtlasJsonModelFactory.createJsonField();
+        field.setPath(fieldPath);
+        field.setFieldType(fieldType);
+        AtlasInternalSession session = read(field);
+        assertEquals(expectedObject, field.getValue());
+        return session;
+    }
+
+    @Test
+    public void testJsonFieldDoubleMax() throws Exception {
+        testBoundaryValue("field-double-max.json", "/doubleValue", FieldType.DOUBLE, Double.MAX_VALUE);
+    }
+
+    @Test
+    public void testJsonFieldDoubleMin() throws Exception {
+        testBoundaryValue("field-double-min.json", "/doubleValue", FieldType.DOUBLE, Double.MIN_VALUE);
+    }
+
+    @Test
+    public void testJsonFieldFloatMax() throws Exception {
+        testBoundaryValue("field-float-max.json", "/floatValue", FieldType.FLOAT, Float.MAX_VALUE);
+    }
+
+    @Test
+    public void testJsonFieldFloatMin() throws Exception {
+        testBoundaryValue("field-float-min.json", "/floatValue", FieldType.FLOAT, Float.MIN_VALUE);
+    }
+
+    @Test
+    public void testJsonFieldLongMax() throws Exception {
+        testBoundaryValue("field-long-max.json", "/longValue", FieldType.LONG, Long.MAX_VALUE);
+    }
+
+    @Test
+    public void testJsonFieldLongMin() throws Exception {
+        testBoundaryValue("field-long-min.json", "/longValue", FieldType.LONG, Long.MIN_VALUE);
+    }
+
+    @Test
+    public void testJsonFieldIntegerMax() throws Exception {
+        testBoundaryValue("field-integer-max.json", "/integerValue", FieldType.INTEGER, Integer.MAX_VALUE);
+    }
+
+    @Test
+    public void testJsonFieldIntegerMin() throws Exception {
+        testBoundaryValue("field-integer-min.json", "/integerValue", FieldType.INTEGER, Integer.MIN_VALUE);
+    }
+
+    @Test
+    public void testJsonFieldShortMax() throws Exception {
+        testBoundaryValue("field-short-max.json", "/shortValue", FieldType.SHORT, Short.MAX_VALUE);
+    }
+
+    @Test
+    public void testJsonFieldShortMin() throws Exception {
+        testBoundaryValue("field-short-min.json", "/shortValue", FieldType.SHORT, Short.MIN_VALUE);
+    }
+
+    @Test
+    public void testJsonFieldCharMax() throws Exception {
+        testBoundaryValue("field-char-max.json", "/charValue", FieldType.CHAR, Character.MAX_VALUE);
+    }
+
+    @Test
+    public void testJsonFieldCharMin() throws Exception {
+        testBoundaryValue("field-char-min.json", "/charValue", FieldType.CHAR, Character.MIN_VALUE);
+    }
+
+    @Test
+    public void testJsonFieldByteMax() throws Exception {
+        testBoundaryValue("field-byte-max.json", "/byteValue", FieldType.BYTE, Byte.MAX_VALUE);
+    }
+
+    @Test
+    public void testJsonFieldByteMin() throws Exception {
+        testBoundaryValue("field-byte-min.json", "/byteValue", FieldType.BYTE, Byte.MIN_VALUE);
+    }
+
+    @Test
+    public void testJsonFieldBooleanTrue() throws Exception {
+        testBoundaryValue("field-boolean-true.json", "/booleanValue", FieldType.BOOLEAN, Boolean.TRUE);
+    }
+
+    @Test
+    public void testJsonFieldBooleanFalse() throws Exception {
+        testBoundaryValue("field-boolean-false.json", "/booleanValue", FieldType.BOOLEAN, Boolean.FALSE);
+    }
+
+    @Test
+    public void testJsonFieldStringNonEmpty() throws Exception {
+        testBoundaryValue("field-string-nonempty.json", "/stringValue", FieldType.STRING, "testString");
+    }
+
+    @Test
+    public void testJsonFieldStringNull() throws Exception {
+        testBoundaryValue("field-string-null.json", "/stringValue", FieldType.STRING, null);
+    }
+
+    @Test
+    public void testJsonFieldStringEmpty() throws Exception {
+        testBoundaryValue("field-string-empty.json", "/stringValue", FieldType.STRING, "");
+    }
+
+    @Test
+    public void testJsonFieldStringNonExist() throws Exception {
+        testBoundaryValue("field-string-nonexist.json", "/stringValue", FieldType.STRING, null);
+    }
+
+    private AtlasInternalSession read(JsonField field) throws AtlasException {
+        AtlasInternalSession session = mock(AtlasInternalSession.class);
+        when(session.head()).thenReturn(mock(Head.class));
+        when(session.head().getSourceField()).thenReturn(field);
+
+        Audits audits = new Audits();
+        when(session.getAudits()).thenReturn(audits);
+        reader.read(session);
+        return session;
+    }
+
+    private void testRangeOutValue(String fileName, String fieldPath, FieldType fieldType, String errorMessage, String errorValue) throws Exception {
+        String filePath = "src" + File.separator + "test" + File.separator + "resources"
+                + File.separator + "jsonFields" + File.separator + fileName;
+        String document = new String(Files.readAllBytes(Paths.get(filePath)));
+        reader.setDocument(document);
+        JsonField field = AtlasJsonModelFactory.createJsonField();
+        field.setPath(fieldPath);
+        field.setFieldType(fieldType);
+        AtlasInternalSession session = read(field);
+
+        assertEquals(null, field.getValue());
+        assertEquals(1, session.getAudits().getAudit().size());
+        assertEquals(errorMessage, session.getAudits().getAudit().get(0).getMessage());
+        assertEquals(errorValue, session.getAudits().getAudit().get(0).getValue());
+        assertEquals(AuditStatus.ERROR, session.getAudits().getAudit().get(0).getStatus());
+    }
+
+    @Test
+    public void testJsonFieldDoubleMaxRangeOut() throws Exception {
+        testRangeOutValue("field-double-max-range-out.json", "/doubleValue", FieldType.DOUBLE, "Failed to convert field value 'Infinity' into type 'DOUBLE'", "Infinity");
+    }
+
+    @Test
+    public void testJsonFieldDoubleMinRangeOut() throws Exception {
+        AtlasInternalSession session = testBoundaryValue("field-double-min-range-out.json", "/doubleValue", FieldType.DOUBLE, 0.0);
+        assertEquals(0, session.getAudits().getAudit().size());
+    }
+
+    @Test
+    public void testJsonFieldFloatMaxRangOut() throws Exception {
+        testRangeOutValue("field-float-max-range-out.json", "/floatValue", FieldType.FLOAT, "Failed to convert field value '3.4028235E39' into type 'FLOAT'", "3.4028235E39");
+    }
+
+    @Test
+    public void testJsonFieldFloatMinRangOut() throws Exception {
+        AtlasInternalSession session = testBoundaryValue("field-float-min-range-out.json", "/floatValue", FieldType.FLOAT, 0.0f);
+        assertEquals(0, session.getAudits().getAudit().size());
+    }
+
+    @Test
+    public void testJsonFieldLongMaxRangeOut() throws Exception {
+        testRangeOutValue("field-long-max-range-out.json", "/longValue", FieldType.LONG, "Failed to convert field value '9223372036854775808' into type 'LONG'", "9223372036854775808");
+    }
+
+    @Test
+    public void testJsonFieldLongMinRangeOut() throws Exception {
+        testRangeOutValue("field-long-min-range-out.json", "/longValue", FieldType.LONG, "Failed to convert field value '-9223372036854775809' into type 'LONG'", "-9223372036854775809");
+    }
+
+    @Test
+    public void testJsonFieldIntegerMaxRangeOut() throws Exception {
+        testRangeOutValue("field-integer-max-range-out.json", "/integerValue", FieldType.INTEGER, "Failed to convert field value '2147483648' into type 'INTEGER'", "2147483648");
+    }
+
+    @Test
+    public void testJsonFieldIntegerMinRangeOut() throws Exception {
+        testRangeOutValue("field-integer-min-range-out.json", "/integerValue", FieldType.INTEGER, "Failed to convert field value '-2147483649' into type 'INTEGER'", "-2147483649");
+    }
+
+    @Test
+    public void testJsonFieldShortMaxRangeOut() throws Exception {
+        testRangeOutValue("field-short-max-range-out.json", "/shortValue", FieldType.SHORT, "Failed to convert field value '32768' into type 'SHORT'", "32768");
+    }
+
+    @Test
+    public void testJsonFieldShortMinRangeOut() throws Exception {
+        testRangeOutValue("field-short-min-range-out.json", "/shortValue", FieldType.SHORT, "Failed to convert field value '-32769' into type 'SHORT'", "-32769");
+    }
+
+    @Test
+    public void testJsonFieldCharMaxRangeOut() throws Exception {
+        testRangeOutValue("field-char-max-range-out.json", "/charValue", FieldType.CHAR, "Failed to convert field value '65536' into type 'CHAR'", "65536");
+    }
+
+    @Test
+    public void testJsonFieldCharMinRangeOut() throws Exception {
+        testRangeOutValue("field-char-min-range-out.json", "/charValue", FieldType.CHAR, "Failed to convert field value '-1' into type 'CHAR'", "-1");
+    }
+
+    @Test
+    public void testJsonFieldByteMaxRangeOut() throws Exception {
+        testRangeOutValue("field-byte-max-range-out.json", "/byteValue", FieldType.BYTE, "Failed to convert field value '128' into type 'BYTE'", "128");
+    }
+
+    @Test
+    public void testJsonFieldByteMinRangeOut() throws Exception {
+        testRangeOutValue("field-byte-min-range-out.json", "/byteValue", FieldType.BYTE, "Failed to convert field value '-129' into type 'BYTE'", "-129");
+    }
+
+    @Test
+    public void testJsonFieldBooleanRangeOut() throws Exception {
+        testBoundaryValue("field-boolean-range-out.json", "/booleanValue", FieldType.BOOLEAN, Boolean.TRUE);
+    }
+
+    @Test
+    public void testJsonFieldBooleanWithLetterF() throws Exception {
+        testBoundaryValue("field-boolean-with-letterF.json", "/booleanValue", FieldType.BOOLEAN, Boolean.FALSE);
+    }
+
+    @Test
+    public void testJsonFieldBooleanWithBlankString() throws Exception {
+        testBoundaryValue("field-boolean-with-blank-string.json", "/booleanValue", FieldType.BOOLEAN, Boolean.FALSE);
+    }
+
+    @Test
+    public void testJsonFieldBooleanWithNull() throws Exception {
+        testBoundaryValue("field-boolean-with-null.json", "/booleanValue", FieldType.BOOLEAN, null);
+    }
+
+    @Test
+    public void testJsonFieldBooleanWithNumber0() throws Exception {
+        testBoundaryValue("field-boolean-with-number0.json", "/booleanValue", FieldType.BOOLEAN, Boolean.FALSE);
+    }
+
+    @Test
+    public void testJsonFieldBooleanWithNumber1() throws Exception {
+        testBoundaryValue("field-boolean-with-number1.json", "/booleanValue", FieldType.BOOLEAN, Boolean.TRUE);
+    }
+
+    @Test
+    public void testJsonFieldLongDecimal() throws Exception {
+        testRangeOutValue("field-long-decimal.json", "/longValue", FieldType.LONG, "Failed to convert field value '9.223372036854776E18' into type 'LONG'", "9.223372036854776E18");
+    }
+
+    @Test
+    public void testJsonFieldIntegerDecimal() throws Exception {
+        testRangeOutValue("field-integer-decimal.json", "/integerValue", FieldType.INTEGER, "Failed to convert field value '2.1474836471234E9' into type 'INTEGER'", "2.1474836471234E9");
+    }
+
+    @Test
+    public void testJsonFieldShortDecimal() throws Exception {
+        testRangeOutValue("field-short-decimal.json", "/shortValue", FieldType.SHORT, "Failed to convert field value '32767.1234' into type 'SHORT'", "32767.1234");
+    }
+
+    @Test
+    public void testJsonFieldCharDecimal() throws Exception {
+        testRangeOutValue("field-char-decimal.json", "/charValue", FieldType.CHAR, "Failed to convert field value '65535.1234' into type 'CHAR'", "65535.1234");
+    }
+
+    @Test
+    public void testJsonFieldByteDecimal() throws Exception {
+        testRangeOutValue("field-byte-decimal.json", "/byteValue", FieldType.BYTE, "Failed to convert field value '127.1234' into type 'BYTE'", "127.1234");
+    }
+
+    @Test
+    public void testJsonFieldDoubleString() throws Exception {
+        testRangeOutValue("field-double-string.json", "/doubleValue", FieldType.DOUBLE, "Failed to convert field value 'abcd' into type 'DOUBLE'", "abcd");
+    }
+
+    @Test
+    public void testJsonFieldFloatString() throws Exception {
+        testRangeOutValue("field-float-string.json", "/floatValue", FieldType.FLOAT, "Failed to convert field value 'abcd' into type 'FLOAT'", "abcd");
+    }
+
+    @Test
+    public void testJsonFieldLongString() throws Exception {
+        testRangeOutValue("field-long-string.json", "/longValue", FieldType.LONG, "Failed to convert field value 'abcd' into type 'LONG'", "abcd");
+    }
+
+    @Test
+    public void testJsonFieldIntegerString() throws Exception {
+        testRangeOutValue("field-integer-string.json", "/integerValue", FieldType.INTEGER, "Failed to convert field value 'abcd' into type 'INTEGER'", "abcd");
+    }
+
+    @Test
+    public void testJsonFieldShortString() throws Exception {
+        testRangeOutValue("field-short-string.json", "/shortValue", FieldType.SHORT, "Failed to convert field value 'abcd' into type 'SHORT'", "abcd");
+    }
+
+    @Test
+    public void testJsonFieldCharString() throws Exception {
+        testRangeOutValue("field-char-string.json", "/charValue", FieldType.CHAR, "Failed to convert field value 'abcd' into type 'CHAR'", "abcd");
+    }
+
+    @Test
+    public void testJsonFieldByteString() throws Exception {
+        testRangeOutValue("field-byte-string.json", "/byteValue", FieldType.BYTE, "Failed to convert field value 'abcd' into type 'BYTE'", "abcd");
+    }
+
 }
