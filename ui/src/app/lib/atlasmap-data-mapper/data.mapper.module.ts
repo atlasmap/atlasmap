b2446fb002fb479dd5f61dc36426a60d0e0503e8
Tomohisa Igarashi <tm.igarashi@gmail.com>
2018-01-29 14:35:18 -0500

    chore: Add document initialization API classes

--- ui/src/app/lib/atlasmap-data-mapper/data.mapper.module.ts
+++ ui/src/app/lib/atlasmap-data-mapper/data.mapper.module.ts
@@ -59,7 +59,7 @@ export { MappingManagementService } from './services/mapping.management.service'
 export { InitializationService } from './services/initialization.service';
 export { DocumentDefinition } from './models/document.definition.model';
 export { MappingDefinition } from './models/mapping.definition.model';
-export { ConfigModel } from './models/config.model';
+export { ConfigModel, DocumentType, InspectionType, DocumentInitializationModel } from './models/config.model';
 export { MappingModel } from './models/mapping.model';
 export { MappingSerializer } from './services/mapping.serializer';
 
