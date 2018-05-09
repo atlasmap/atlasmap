b1e1078f85b39ddfbe4212b87a185026cc0bfe4e
John Verhaeg <verhaeg@icloud.com>
2018-05-09 00:51:13 -0500

    Fix for #405: Add UI tests for fixes to field action availability

--- ui/src/app/lib/atlasmap-data-mapper/models/transition.model.spec.ts
+++ ui/src/app/lib/atlasmap-data-mapper/models/transition.model.spec.ts
@@ -1,7 +1,9 @@
 /* tslint:disable:no-unused-variable */
 
 import { TestBed, async, inject } from '@angular/core/testing';
-import { TransitionModel, FieldActionConfig } from './transition.model';
+import { FieldActionConfig, TransitionModel } from './transition.model';
+import { FieldMappingPair } from './mapping.model';
+import { Field } from './field.model';
 
 describe('TransitionModel', () => {
   beforeEach(() => {
@@ -18,13 +20,81 @@ describe('TransitionModel', () => {
   );
 });
 
-describe('FieldActionConfig.appliesToField', () => {
-    let cfg: FieldActionConfig;
+describe('FieldActionConfig.appliesToField()', () => {
+    let action: FieldActionConfig;
+    let pair: FieldMappingPair;
+    let source: Field;
+    let target: Field;
     beforeEach(() => {
-      cfg = new FieldActionConfig();
+      action = new FieldActionConfig();
+      pair = new FieldMappingPair();
+      pair.sourceFields.splice(0);
+      source = new Field();
+      pair.addField(source, true);
+      pair.targetFields.splice(0);
+      target = new Field();
+      pair.addField(target, false);
     });
 
-    it('#appliesToField should return false', () => {
-      expect(cfg.appliesToField(null)).toBe(false);
+    it('should return false if FieldMappingPair is null', () => {
+      expect(action.appliesToField(null)).toBe(false);
+    });
+
+    it('should return false if source or target is null', () => {
+      pair.targetFields.splice(0);
+      expect(action.appliesToField(pair)).toBe(false);
+    });
+
+    it('should return false if action collection type is ALL, target collection type is not ARRAY or LIST, and type type is not \
+       STRING', () => {
+      action.serviceObject.sourceCollectionType = 'ALL';
+      expect(action.appliesToField(pair)).toBe(false);
+    });
+
+    it('should return false if action collection type is NONE and target field collection type is not null', () => {
+      action.serviceObject.sourceCollectionType = 'NONE';
+      target.isCollection = true;
+      expect(action.appliesToField(pair)).toBe(false);
+    });
+
+    it('should return false if action source collection type does not match target collection type', () => {
+      action.serviceObject.sourceCollectionType = 'LIST';
+      action.serviceObject.targetCollectionType = 'NONE';
+      expect(action.appliesToField(pair)).toBe(false);
+    });
+
+    it('should return if action target type is NUMBER and target field type is numeric', () => {
+      action.targetType = 'NUMBER';
+      target.type = 'SHORT';
+      expect(action.appliesToField(pair)).toBe(true);
+      target.type = 'NUMBER';
+      expect(action.appliesToField(pair)).toBe(true);
+      target.type = 'STRING';
+      expect(action.appliesToField(pair)).toBe(false);
+    });
+
+    it('should return if action target type is ANY_DATE and target field type is a date/time', () => {
+      action.targetType = 'ANY_DATE';
+      target.type = 'DATE';
+      expect(action.appliesToField(pair)).toBe(true);
+      target.type = 'DATE_TIME_TZ';
+      expect(action.appliesToField(pair)).toBe(true);
+      target.type = 'STRING';
+      expect(action.appliesToField(pair)).toBe(false);
+    });
+
+    it('should return if action source type is ANY or matches target field type, and action target type matches target type',
+       () => {
+      target.type = 'STRING';
+      action.targetType = 'STRING';
+      action.sourceType = 'ANY';
+      expect(action.appliesToField(pair)).toBe(true);
+      action.sourceType = 'STRING';
+      expect(action.appliesToField(pair)).toBe(true);
+      action.sourceType = 'CHAR';
+      expect(action.appliesToField(pair)).toBe(false);
+      action.sourceType = 'STRING';
+      target.type = 'CHAR';
+      expect(action.appliesToField(pair)).toBe(false);
     });
 });
