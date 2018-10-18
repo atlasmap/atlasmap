7a6f3f939e01cd842269add93fa236dfbb2434ca
Paul Leacu <pleacu@redhat.com>
2018-10-18 17:14:25 -0400

    fix: Leave combine/separate elements in index order - not alphabetical order. Fixes: #624

--- ui/src/app/lib/atlasmap-data-mapper/components/mapping/mapping-list.component.ts
+++ ui/src/app/lib/atlasmap-data-mapper/components/mapping/mapping-list.component.ts
@@ -71,7 +71,6 @@ export class MappingListComponent {
 
   getMappedFields(fieldPair: FieldMappingPair, isSource: boolean): MappedField[] {
     let fields: MappedField[] = fieldPair.getMappedFields(isSource);
-    fields = MappedField.sortMappedFieldsByPath(fields, false);
     if (fields.length === 0) {
       const mappedField: MappedField = new MappedField();
       mappedField.field = DocumentDefinition.getNoneField();
