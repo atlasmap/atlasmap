8056385f5f8b557e11f60ddb0cc8c4611a88d7cf
Abbass Faytaroony <abbassfaytaroony@gmail.com>
2019-05-19 00:46:11 +0300

    - Added routing, with default route

---
+++ ui/src/app/app-routing.module.ts
@@ -0,0 +1,25 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import {DataMapperAppExampleHostComponent} from "./lib/atlasmap-data-mapper/components/data-mapper-example-host.component";
+
+const routes: Routes = [
+  {
+    path:'mapping-id/:id',
+    component: DataMapperAppExampleHostComponent
+  },
+  {
+    path:'',
+    component: DataMapperAppExampleHostComponent
+  }
+];
+
+@NgModule({
+  imports: [
+    RouterModule.forRoot(routes)
+  ],
+  exports: [
+    RouterModule
+  ],
+  declarations: []
+})
+export class AppRoutingModule { }
--- ui/src/app/app.component.ts
+++ ui/src/app/app.component.ts
@@ -18,7 +18,7 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'atlasmap-dev-root',
-  template: '<data-mapper-example-host></data-mapper-example-host>',
+  template: '<router-outlet></router-outlet>',
 })
 
-export class AppComponent { }
+export class AppComponent {}
