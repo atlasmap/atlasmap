d67feb9286ec64a24a68fc0d5921070daa448c74
Tomohisa Igarashi <tm.igarashi@gmail.com>
2019-09-19 21:29:25 -0400

    fix: InitializationService UT is not working
    Fixes: #1226

--- ui/src/app/lib/atlasmap-data-mapper/services/initialization.service.spec.ts
+++ ui/src/app/lib/atlasmap-data-mapper/services/initialization.service.spec.ts
@@ -1,10 +1,10 @@
 /* tslint:disable:no-unused-variable */
 
-import { TestBed, async, inject } from '@angular/core/testing';
+import { TestBed, inject } from '@angular/core/testing';
 import { HttpClientModule } from '@angular/common/http';
 import { HttpClientTestingModule } from '@angular/common/http/testing';
 import { LoggerModule, NGXLogger, NgxLoggerLevel } from 'ngx-logger';
-import { DocumentType } from '../common/config.types';
+import { DocumentType, InspectionType } from '../common/config.types';
 import { DocumentManagementService } from './document-management.service';
 import { ErrorHandlerService } from './error-handler.service';
 import { InitializationService } from './initialization.service';
@@ -12,11 +12,16 @@ import { MappingManagementService } from './mapping-management.service';
 import { FieldActionService } from './field-action.service';
 import { FileManagementService } from './file-management.service';
 import { DocumentInitializationModel } from '../models/config.model';
+import { BrowserDynamicTestingModule, platformBrowserDynamicTesting } from '@angular/platform-browser-dynamic/testing';
 
 describe('InitializationService', () => {
   beforeEach(() => {
+    TestBed.resetTestEnvironment();
+    TestBed.initTestEnvironment(BrowserDynamicTestingModule,
+      platformBrowserDynamicTesting());
+
     TestBed.configureTestingModule({
-      imports: [ HttpClientModule, HttpClientTestingModule, LoggerModule.forRoot({level: NgxLoggerLevel.DEBUG}) ],
+      imports: [HttpClientModule, HttpClientTestingModule, LoggerModule.forRoot({ level: NgxLoggerLevel.DEBUG })],
       providers: [
         DocumentManagementService,
         ErrorHandlerService,
@@ -31,35 +36,47 @@ describe('InitializationService', () => {
   });
 
   it(
-    'should load document definitions',
-     async(inject([InitializationService], (service: InitializationService) => {
-      const c = service.cfg;
-      const sourceJson = new DocumentInitializationModel();
-      sourceJson.isSource = true;
-      sourceJson.type = DocumentType.JSON;
-      sourceJson.inspectionResult = jasmine.getFixtures().read('atlasmap-inspection-mock-json-schema.json');
-      c.addDocument(sourceJson);
-      const sourceXml = new DocumentInitializationModel();
-      sourceXml.isSource = true;
-      sourceJson.type = DocumentType.XML;
-      sourceXml.inspectionResult = jasmine.getFixtures().read('atlasmap-inspection-mock-xml-instance-1.json');
-      c.addDocument(sourceJson);
-      const targetJson = new DocumentInitializationModel();
-      targetJson.isSource = false;
-      sourceJson.type = DocumentType.JSON;
-      targetJson.inspectionResult = jasmine.getFixtures().read('atlasmap-inspection-mock-json-instance.json');
-      c.addDocument(sourceJson);
-      const targetXml = new DocumentInitializationModel();
-      targetJson.isSource = false;
-      sourceJson.type = DocumentType.XML;
-      targetXml.inspectionResult = jasmine.getFixtures().read('atlasmap-inspection-mock-xml-schema-1.json');
-      c.addDocument(sourceJson);
-      service.initialize().then(() => {
-        expect(c.sourceDocs[0].fields[0].path).toEqual('/order');
-        expect(c.sourceDocs[1].fields[0].path).toEqual('/data');
-        expect(c.targetDocs[0].fields[0].path).toEqual('/order');
-        expect(c.targetDocs[1].fields[0].path).toEqual('/data');
-      });
-    }),
-  ));
+    'should load document definitions', (done) => {
+      inject([InitializationService], (service: InitializationService) => {
+        const c = service.cfg;
+        c.initCfg.baseJSONInspectionServiceUrl = 'dummy';
+        c.initCfg.baseXMLInspectionServiceUrl = 'dummy';
+        const sourceJson = new DocumentInitializationModel();
+        sourceJson.isSource = true;
+        sourceJson.type = DocumentType.JSON;
+        sourceJson.inspectionType = InspectionType.SCHEMA;
+        sourceJson.inspectionResult = jasmine.getFixtures().read('atlasmap-inspection-mock-json-schema.json');
+        c.addDocument(sourceJson);
+        const sourceXml = new DocumentInitializationModel();
+        sourceXml.isSource = true;
+        sourceXml.type = DocumentType.XML;
+        sourceXml.inspectionType = InspectionType.INSTANCE;
+        sourceXml.inspectionResult = jasmine.getFixtures().read('atlasmap-inspection-mock-xml-instance-1.json');
+        c.addDocument(sourceXml);
+        const targetJson = new DocumentInitializationModel();
+        targetJson.isSource = false;
+        targetJson.type = DocumentType.JSON;
+        targetJson.inspectionType = InspectionType.INSTANCE;
+        targetJson.inspectionResult = jasmine.getFixtures().read('atlasmap-inspection-mock-json-instance.json');
+        c.addDocument(targetJson);
+        const targetXml = new DocumentInitializationModel();
+        targetXml.isSource = false;
+        targetXml.type = DocumentType.XML;
+        targetXml.inspectionType = InspectionType.SCHEMA;
+        targetXml.inspectionResult = jasmine.getFixtures().read('atlasmap-inspection-mock-xml-schema-1.json');
+        c.addDocument(targetXml);
+        spyOn(c.mappingService, 'runtimeServiceActive').and.returnValues(true);
+        return service.initialize().then(() => {
+          expect(c.sourceDocs[0].fields[0].path).toEqual('/addressList<>');
+          expect(c.sourceDocs[1].fields[0].path).toEqual('/data');
+          expect(c.targetDocs[0].fields[0].path).toEqual('/addressList<>');
+          expect(c.targetDocs[1].fields[0].path).toEqual('/data');
+          done();
+        }).catch((error) => {
+          fail(error);
+          done();
+        });
+      })();
+    });
+
 });
