71b6a2e7278f59823742d66693b2ed7df8f93df4
Tomohisa Igarashi <tm.igarashi@gmail.com>
2019-11-25 14:57:25 -0500

    chore: Update to Camel-2.23 + SpringBoot 2.1
    Fixes: #756

--- standalone/src/main/java/io/atlasmap/standalone/Application.java
+++ standalone/src/main/java/io/atlasmap/standalone/Application.java
@@ -19,7 +19,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.web.support.SpringBootServletInitializer;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.context.ConfigurableApplicationContext;
 
 @SpringBootApplication
