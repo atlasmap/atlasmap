7d3699cdb30bf6152e5917a01f0229738fff3a3b
John Verhaeg <verhaeg@icloud.com>
2020-02-17 23:08:44 -0600

    fix: UI_REACT: Change toolbar menu separators to spaces

--- ui-react/packages/atlasmap-ui/src/AtlasmapUI/AtlasmapContextToolbar.tsx
+++ ui-react/packages/atlasmap-ui/src/AtlasmapUI/AtlasmapContextToolbar.tsx
@@ -13,16 +13,16 @@ import {
   ExpandIcon,
 } from '@patternfly/react-icons';
 import React, { FunctionComponent } from 'react';
-import { Button, Tooltip } from '@patternfly/react-core';
+import {
+  Button,
+  Tooltip,
+  Toolbar,
+  ToolbarItem,
+  ToolbarGroup,
+} from '@patternfly/react-core';
 
 import { FilePicker } from 'react-file-picker';
 import { css, StyleSheet } from '@patternfly/react-styles';
-import {
-  DataToolbarItem,
-  DataToolbar,
-  DataToolbarContent,
-  DataToolbarGroup,
-} from '@patternfly/react-core/dist/js/experimental';
 
 const styles = StyleSheet.create({
   toolbar: { borderBottom: '1px solid #ccc' },
@@ -58,223 +58,211 @@ export const AtlasmapContextToolbar: FunctionComponent<IAtlasmapContextToolbarPr
   showFreeView,
 }) => {
   return (
-    <DataToolbar
+    <Toolbar
       id="data-toolbar"
       className={css('view-toolbar pf-u-px-md pf-u-py-md', styles.toolbar)}
     >
-      <DataToolbarContent>
-        <React.Fragment>
-          <DataToolbarItem>
+      <ToolbarItem>
+        <Tooltip
+          position={'auto'}
+          enableFlip={true}
+          content={
+            <div>
+              Import an AtlasMap mappings catalog file (.adm) or Java archive
+              (.jar).
+            </div>
+          }
+        >
+          <FilePicker
+            extensions={['adm', 'jar']}
+            onChange={onImportAtlasFile}
+            onError={(errMsg: any) => console.error(errMsg)}
+          >
+            <Button variant={'plain'} aria-label="Import mappings">
+              <ImportIcon />
+            </Button>
+          </FilePicker>
+        </Tooltip>
+      </ToolbarItem>
+      <ToolbarItem>
+        <Tooltip
+          position={'auto'}
+          enableFlip={true}
+          content={
+            <div>
+              Export the current mappings and support files into a catalog
+              (.adm) file.
+            </div>
+          }
+        >
+          <Button
+            variant={'plain'}
+            aria-label="Export mappings"
+            onClick={(event: any) => onExportAtlasFile(event)}
+          >
+            <ExportIcon />
+          </Button>
+        </Tooltip>
+      </ToolbarItem>
+      <ToolbarItem>
+        <Tooltip
+          position={'auto'}
+          enableFlip={true}
+          content={
+            <div>Reset all mappings and clear all imported documents</div>
+          }
+        >
+          <Button
+            variant={'plain'}
+            aria-label="Reset all"
+            onClick={onResetAtlasmap}
+          >
+            Reset all
+          </Button>
+        </Tooltip>
+      </ToolbarItem>
+      <ToolbarItem>
+        <Tooltip
+          position={'auto'}
+          enableFlip={true}
+          content={<div>Show/hide mapping preview</div>}
+        >
+          <Button
+            variant={'plain'}
+            aria-label="Show/hide mapping preview"
+            onClick={onToggleShowMappingPreview}
+          >
+            <EyeIcon />
+          </Button>
+        </Tooltip>
+      </ToolbarItem>
+      <ToolbarItem>
+        <Tooltip
+          position={'auto'}
+          enableFlip={true}
+          content={<div>Show/hide mapping table</div>}
+        >
+          <Button variant={'plain'} aria-label="Show/hide mapping table">
+            <TableIcon />
+          </Button>
+        </Tooltip>
+      </ToolbarItem>
+      <ToolbarItem>
+        <Tooltip
+          position={'auto'}
+          enableFlip={true}
+          content={<div>Show/hide namespace table</div>}
+        >
+          <Button variant={'plain'} aria-label="Show/hide namespace table">
+            <CodeIcon />
+          </Button>
+        </Tooltip>
+      </ToolbarItem>
+      <ToolbarItem>
+        <Tooltip
+          position={'auto'}
+          enableFlip={true}
+          content={<div>Show/hide types</div>}
+        >
+          <Button
+            variant={'plain'}
+            aria-label="Show/hide types"
+            onClick={onToggleShowTypes}
+          >
+            <InfoIcon />
+          </Button>
+        </Tooltip>
+      </ToolbarItem>
+      <ToolbarItem>
+        <Tooltip
+          position={'auto'}
+          enableFlip={true}
+          content={<div>Show/hide mapped fields</div>}
+        >
+          <Button
+            variant={'plain'}
+            aria-label="Show/hide mapped fields"
+            onClick={onToggleShowMappedFields}
+          >
+            <MapMarkedIcon />
+          </Button>
+        </Tooltip>
+      </ToolbarItem>
+      <ToolbarItem>
+        <Tooltip
+          position={'auto'}
+          enableFlip={true}
+          content={<div>Show/hide unmapped fields</div>}
+        >
+          <Button
+            variant={'plain'}
+            aria-label="Show/hide unmapped fields"
+            onClick={onToggleShowUnmappedFields}
+          >
+            <MapIcon />
+          </Button>
+        </Tooltip>
+      </ToolbarItem>
+      <ToolbarItem>
+        <Tooltip
+          position={'auto'}
+          enableFlip={true}
+          content={<div>Show/hide free view</div>}
+        >
+          <Button
+            variant={'plain'}
+            aria-label="Show/hide free view"
+            onClick={onToggleShowFreeView}
+          >
+            <PficonDragdropIcon />
+          </Button>
+        </Tooltip>
+      </ToolbarItem>
+      {showFreeView && (
+        <ToolbarGroup>
+          <ToolbarItem>
             <Tooltip
               position={'auto'}
               enableFlip={true}
-              content={
-                <div>
-                  Import an AtlasMap mappings catalog file (.adm) or Java
-                  archive (.jar).
-                </div>
-              }
+              content={<div>Zoom in</div>}
             >
-              <FilePicker
-                extensions={['adm', 'jar']}
-                onChange={onImportAtlasFile}
-                onError={(errMsg: any) => console.error(errMsg)}
-              >
-                <Button variant={'plain'} aria-label="Import mappings">
-                  <ImportIcon />
-                </Button>
-              </FilePicker>
+              <Button variant={'plain'} aria-label="Zoom in" onClick={onZoomIn}>
+                <SearchPlusIcon />
+              </Button>
             </Tooltip>
-          </DataToolbarItem>
-          <DataToolbarItem>
+          </ToolbarItem>
+          <ToolbarItem>
             <Tooltip
               position={'auto'}
               enableFlip={true}
-              content={
-                <div>
-                  Export the current mappings and support files into a catalog
-                  (.adm) file.
-                </div>
-              }
+              content={<div>Zoom out</div>}
             >
               <Button
                 variant={'plain'}
-                aria-label="Export mappings"
-                onClick={(event: any) => onExportAtlasFile(event)}
+                aria-label="Zoom out"
+                onClick={onZoomOut}
               >
-                <ExportIcon />
+                <SearchMinusIcon />
               </Button>
             </Tooltip>
-          </DataToolbarItem>
-          <DataToolbarItem variant="separator"></DataToolbarItem>
-          <DataToolbarItem>
+          </ToolbarItem>
+          <ToolbarItem>
             <Tooltip
               position={'auto'}
               enableFlip={true}
-              content={
-                <div>Reset all mappings and clear all imported documents</div>
-              }
+              content={<div>Reset view</div>}
             >
               <Button
                 variant={'plain'}
-                aria-label="Reset all"
-                onClick={onResetAtlasmap}
+                aria-label="Reset view"
+                onClick={onResetView}
               >
-                Reset all
+                <ExpandIcon />
               </Button>
             </Tooltip>
-          </DataToolbarItem>
-          <DataToolbarItem variant="separator"></DataToolbarItem>
-          <DataToolbarItem>
-            <Tooltip
-              position={'auto'}
-              enableFlip={true}
-              content={<div>Show/hide mapping preview</div>}
-            >
-              <Button
-                variant={'plain'}
-                aria-label="Show/hide mapping preview"
-                onClick={onToggleShowMappingPreview}
-              >
-                <EyeIcon />
-              </Button>
-            </Tooltip>
-          </DataToolbarItem>
-          <DataToolbarItem>
-            <Tooltip
-              position={'auto'}
-              enableFlip={true}
-              content={<div>Show/hide mapping table</div>}
-            >
-              <Button variant={'plain'} aria-label="Show/hide mapping table">
-                <TableIcon />
-              </Button>
-            </Tooltip>
-          </DataToolbarItem>
-          <DataToolbarItem>
-            <Tooltip
-              position={'auto'}
-              enableFlip={true}
-              content={<div>Show/hide namespace table</div>}
-            >
-              <Button variant={'plain'} aria-label="Show/hide namespace table">
-                <CodeIcon />
-              </Button>
-            </Tooltip>
-          </DataToolbarItem>
-          <DataToolbarItem variant="separator"></DataToolbarItem>
-          <DataToolbarItem>
-            <Tooltip
-              position={'auto'}
-              enableFlip={true}
-              content={<div>Show/hide types</div>}
-            >
-              <Button
-                variant={'plain'}
-                aria-label="Show/hide types"
-                onClick={onToggleShowTypes}
-              >
-                <InfoIcon />
-              </Button>
-            </Tooltip>
-          </DataToolbarItem>
-          <DataToolbarItem>
-            <Tooltip
-              position={'auto'}
-              enableFlip={true}
-              content={<div>Show/hide mapped fields</div>}
-            >
-              <Button
-                variant={'plain'}
-                aria-label="Show/hide mapped fields"
-                onClick={onToggleShowMappedFields}
-              >
-                <MapMarkedIcon />
-              </Button>
-            </Tooltip>
-          </DataToolbarItem>
-          <DataToolbarItem>
-            <Tooltip
-              position={'auto'}
-              enableFlip={true}
-              content={<div>Show/hide unmapped fields</div>}
-            >
-              <Button
-                variant={'plain'}
-                aria-label="Show/hide unmapped fields"
-                onClick={onToggleShowUnmappedFields}
-              >
-                <MapIcon />
-              </Button>
-            </Tooltip>
-          </DataToolbarItem>
-          <DataToolbarItem variant="separator"></DataToolbarItem>
-          <DataToolbarItem>
-            <Tooltip
-              position={'auto'}
-              enableFlip={true}
-              content={<div>Show/hide free view</div>}
-            >
-              <Button
-                variant={'plain'}
-                aria-label="Show/hide free view"
-                onClick={onToggleShowFreeView}
-              >
-                <PficonDragdropIcon />
-              </Button>
-            </Tooltip>
-          </DataToolbarItem>
-          {showFreeView && (
-            <DataToolbarGroup>
-              <DataToolbarItem>
-                <Tooltip
-                  position={'auto'}
-                  enableFlip={true}
-                  content={<div>Zoom in</div>}
-                >
-                  <Button
-                    variant={'plain'}
-                    aria-label="Zoom in"
-                    onClick={onZoomIn}
-                  >
-                    <SearchPlusIcon />
-                  </Button>
-                </Tooltip>
-              </DataToolbarItem>
-              <DataToolbarItem>
-                <Tooltip
-                  position={'auto'}
-                  enableFlip={true}
-                  content={<div>Zoom out</div>}
-                >
-                  <Button
-                    variant={'plain'}
-                    aria-label="Zoom out"
-                    onClick={onZoomOut}
-                  >
-                    <SearchMinusIcon />
-                  </Button>
-                </Tooltip>
-              </DataToolbarItem>
-              <DataToolbarItem>
-                <Tooltip
-                  position={'auto'}
-                  enableFlip={true}
-                  content={<div>Reset view</div>}
-                >
-                  <Button
-                    variant={'plain'}
-                    aria-label="Reset view"
-                    onClick={onResetView}
-                  >
-                    <ExpandIcon />
-                  </Button>
-                </Tooltip>
-              </DataToolbarItem>
-            </DataToolbarGroup>
-          )}
-        </React.Fragment>
-      </DataToolbarContent>
-    </DataToolbar>
+          </ToolbarItem>
+        </ToolbarGroup>
+      )}
+    </Toolbar>
   );
 };
