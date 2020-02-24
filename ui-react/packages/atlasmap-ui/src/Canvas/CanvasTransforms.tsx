208c382dc60d55ce4ba2f4fb40e2fd631053fab0
John Verhaeg <verhaeg@icloud.com>
2020-02-24 09:21:04 -0600

    fix: UI_REACT: Deselect current mapping when background clicked (#1807)

--- ui-react/packages/atlasmap-ui/src/Canvas/CanvasTransforms.tsx
+++ ui-react/packages/atlasmap-ui/src/Canvas/CanvasTransforms.tsx
@@ -1,5 +1,6 @@
-import React, { FunctionComponent } from 'react';
+import React, { FunctionComponent, useCallback } from 'react';
 import { useCanvas } from './CanvasContext';
+import { useAtlasmapUI } from '../AtlasmapUI/AtlasmapUIProvider';
 
 export const CanvasTransforms: FunctionComponent = ({ children }) => {
   const { panX, panY, zoom /* width, height, xDomain, yDomain*/ } = useCanvas();
@@ -7,12 +8,26 @@ export const CanvasTransforms: FunctionComponent = ({ children }) => {
   // const originY = height / 2;
   // const x = xDomain(zoom * (panX + originX) - originX);
   // const y = yDomain(zoom * (panY + originY) - originY);
+  const {
+    deselectMapping,
+    closeMappingDetails,
+    selectedMapping,
+  } = useAtlasmapUI();
+
+  const handleSelect = useCallback(() => {
+    if (selectedMapping) {
+      closeMappingDetails();
+      deselectMapping();
+    }
+  }, [selectedMapping, closeMappingDetails, deselectMapping]);
+
   return (
     <g
       transform={`
         translate(${panX} ${panY})
         scale(${zoom})
       `}
+      onClick={handleSelect}
     >
       {children}
     </g>
