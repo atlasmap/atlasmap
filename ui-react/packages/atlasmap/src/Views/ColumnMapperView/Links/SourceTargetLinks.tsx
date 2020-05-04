2fc8c80084c9107b84db8b24e56f709fe433928c
Riccardo Forina <rforina@redhat.com>
2020-05-04 16:02:17 +0200

    fix(react-ui): improve some performance bottlenecks

--- ui-react/packages/atlasmap/src/Views/ColumnMapperView/Links/SourceTargetLinks.tsx
+++ ui-react/packages/atlasmap/src/Views/ColumnMapperView/Links/SourceTargetLinks.tsx
@@ -1,4 +1,9 @@
-import React, { FunctionComponent, MouseEvent, ReactElement } from "react";
+import React, {
+  FunctionComponent,
+  MouseEvent,
+  ReactElement,
+  useMemo,
+} from "react";
 
 import { css, StyleSheet } from "@patternfly/react-styles";
 
@@ -15,57 +20,51 @@ const styles = StyleSheet.create({
 export interface ISourceTargetLinksProps {
   mappings: IAtlasmapMapping[];
   selectedMappingId?: string;
-  highlightedMappingId?: string;
-  onMouseOver: (mapping: IAtlasmapMapping) => void;
-  onMouseOut: () => void;
   onSelectMapping: (mapping: IAtlasmapMapping) => void;
 }
 
 export const SourceTargetLinks: FunctionComponent<ISourceTargetLinksProps> = ({
   mappings,
   selectedMappingId,
-  highlightedMappingId,
   onSelectMapping,
-  onMouseOver,
-  onMouseOut,
 }) => {
-  const links = mappings.reduce<ReactElement[]>((lines, m) => {
-    const handleMouseOver = () => onMouseOver(m);
-    const handleMouseOut = () => onMouseOut();
-    const handleClick = (event: MouseEvent) => {
-      onSelectMapping(m);
-      event.stopPropagation();
-    };
+  const links = useMemo(() => {
+    return mappings.reduce<ReactElement[]>((lines, m) => {
+      const handleClick = (event: MouseEvent) => {
+        onSelectMapping(m);
+        event.stopPropagation();
+      };
 
-    const isHighlighted = m.id === highlightedMappingId;
-    const isSelected = m.id === selectedMappingId;
-    const color = isSelected
-      ? "var(--pf-global--active-color--100)"
-      : isHighlighted
-      ? "var(--pf-global--active-color--400)"
-      : undefined;
-    const mappingLines = m.sourceFields.reduce<ReactElement[]>(
-      (lines, start) => {
-        const linesFromSource = m.targetFields.map((end) => (
-          <NodesArc
-            key={`${start.id}${end.id}`}
-            start={`${SOURCES_FIELD_ID_PREFIX}${start.id}`}
-            end={`${TARGETS_FIELD_ID_PREFIX}${end.id}`}
-            color={color}
-            onClick={handleClick}
-            onMouseOver={handleMouseOver}
-            onMouseOut={handleMouseOut}
-            className={css(styles.arc)}
-          />
-        ));
-        return [...lines, ...linesFromSource];
-      },
-      [],
-    );
-    return isSelected
-      ? [...lines, ...mappingLines]
-      : [...mappingLines, ...lines];
-  }, []);
+      const isSelected = m.id === selectedMappingId;
+      const color = isSelected
+        ? "var(--pf-global--active-color--100)"
+        : undefined;
+      const hoverColor = !isSelected
+        ? "var(--pf-global--active-color--400)"
+        : undefined;
+      const mappingLines = m.sourceFields.reduce<ReactElement[]>(
+        (lines, start) => {
+          const linesFromSource = m.targetFields.map((end) => (
+            <NodesArc
+              key={`${start.id}${end.id}`}
+              start={`${SOURCES_FIELD_ID_PREFIX}${start.id}`}
+              end={`${TARGETS_FIELD_ID_PREFIX}${end.id}`}
+              color={color}
+              hoveredColor={hoverColor}
+              onClick={handleClick}
+              className={css(styles.arc)}
+            />
+          ));
+          return [...lines, ...linesFromSource];
+        },
+        [],
+      );
+      return isSelected
+        ? [...lines, ...mappingLines]
+        : [...mappingLines, ...lines];
+    }, []);
+  }, [mappings, onSelectMapping, selectedMappingId]);
+
   return (
     <>
       {links}
