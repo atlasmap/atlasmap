6095eb4b175548869d28464c867f375dfbf4f572
Tomohisa Igarashi <tm.igarashi@gmail.com>
2020-05-22 12:55:54 -0400

    chore: Attach upstream master coverage report to developer guide
    Fixes: #2098
    
    chore: downgrade maven-javadoc-plugin to restore expected aggregate behavior
    Fixes: #2103

--- docs/publish-docs.sh
+++ docs/publish-docs.sh
@@ -3,22 +3,31 @@ echo ================================
 echo Deploying AtlasMap documentation
 echo ================================
 
-../mvnw -Phtml,pdf package && \
-../mvnw -f pom-javadoc.xml -pl \!io.atlasmap:atlasmap-lib-all javadoc:aggregate && \
-git clone -b gh-pages https://github.com/atlasmap/atlasmap.git gh-pages && \
-git config --global user.email "travis@atlasmap.io" && \
-git config --global user.name "Travis" && \
-cd gh-pages && \
-git rm -r * && \
-cd .. && \
-cp src/main/resources/CNAME gh-pages/ && \
-cp -rv target/generated-docs/html/user-guide/* gh-pages/ && \
-cp -v target/generated-docs/pdf/user-guide/index.pdf gh-pages/user-guide.pdf && \
+cd ..
+./mvnw -Pcoverage install
+cd docs
+../mvnw -Phtml,pdf package
+../mvnw -f pom-javadoc.xml -pl \!io.atlasmap:atlasmap-lib-all javadoc:aggregate
+mkdir -p target/generated-docs/html/developer-guide/coverage/{java,ui}
+cp -a ../coverage-report/target/site/jacoco-aggregate/* target/generated-docs/html/developer-guide/coverage/java
+for dir in `ls --color=never ../ui/packages`; do
+  mkdir target/generated-docs/html/developer-guide/coverage/ui/${dir}
+  cp -a ../ui/packages/${dir}/coverage/lcov-report/* target/generated-docs/html/developer-guide/coverage/ui/${dir}/ 2>/dev/null || :
+done
+git clone -b gh-pages https://github.com/atlasmap/atlasmap.git gh-pages
+git config --global user.email "travis@atlasmap.io"
+git config --global user.name "Travis"
+cd gh-pages
+git rm -r *
+cd ..
+cp src/main/resources/CNAME gh-pages/
+cp -rv target/generated-docs/html/user-guide/* gh-pages/
+cp -v target/generated-docs/pdf/user-guide/index.pdf gh-pages/user-guide.pdf
 mkdir -p gh-pages/developer-guide
-cp -rv target/generated-docs/html/developer-guide/* gh-pages/developer-guide/ && \
-cp -v target/generated-docs/pdf/developer-guide/index.pdf gh-pages/developer-guide.pdf && \
-cd gh-pages && \
-git add --ignore-errors * && \
-git commit -m "generated documentation" && \
-git push origin gh-pages && \
+cp -rv target/generated-docs/html/developer-guide/* gh-pages/developer-guide/
+cp -v target/generated-docs/pdf/developer-guide/index.pdf gh-pages/developer-guide.pdf
+cd gh-pages
+git add --ignore-errors *
+git commit -m "generated documentation"
+git push origin gh-pages
 cd ..
