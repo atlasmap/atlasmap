e989545df75ce69f6d859c4e0a94b363841c3609
pleacu <pleacu@redhat.com>
2020-06-04 14:15:02 -0400

    Correct custom field action maven tests Fixes: #2161

--- atlasmap-maven-plugin/src/test/java/io/atlasmap/maven/test/PaulsFieldActions.java
+++ atlasmap-maven-plugin/src/test/java/io/atlasmap/maven/test/PaulsFieldActions.java
@@ -1,20 +1,14 @@
 package io.atlasmap.maven.test;
 
-import java.io.Serializable;
-
-import com.fasterxml.jackson.annotation.JsonPropertyDescription;
-
 import io.atlasmap.spi.AtlasFieldAction;
 import io.atlasmap.spi.AtlasActionProcessor;
-import io.atlasmap.v2.Action;
-import io.atlasmap.v2.AtlasActionProperty;
-import io.atlasmap.v2.FieldType;
+import io.atlasmap.v2.*;
 
 public class PaulsFieldActions implements AtlasFieldAction {
 
     @AtlasActionProcessor
     public static String myCustomFieldAction(PaulsFieldActionsModel myCustomFieldAction, String input) {
-        return "Paul's custom field action: " + myCustomFieldAction.getPaulsParam() + " payload: " + input;
+        return myCustomFieldAction.getPaulsParam() + input;
     }
 
 }
