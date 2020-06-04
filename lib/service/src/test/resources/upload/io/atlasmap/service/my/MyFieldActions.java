d1297082200a828f325ff5f181e2cb85e75b1c4b
Tomohisa Igarashi <tm.igarashi@gmail.com>
2020-06-04 15:20:49 -0400

    chore: Update custom action upload test to use AtlasActionProcessor

--- lib/service/src/test/resources/upload/io/atlasmap/service/my/MyFieldActions.java
+++ lib/service/src/test/resources/upload/io/atlasmap/service/my/MyFieldActions.java
@@ -1,15 +1,15 @@
 package io.atlasmap.service.my;
 
 import io.atlasmap.spi.AtlasFieldAction;
-import io.atlasmap.spi.AtlasFieldActionInfo;
+import io.atlasmap.spi.AtlasActionProcessor;
 import io.atlasmap.v2.Action;
 import io.atlasmap.v2.CollectionType;
 import io.atlasmap.v2.FieldType;
 
 public class MyFieldActions implements AtlasFieldAction {
 
-    @AtlasFieldActionInfo(name = "MyCustomFieldAction", sourceType = FieldType.STRING, targetType = FieldType.STRING, sourceCollectionType = CollectionType.NONE, targetCollectionType = CollectionType.NONE)
-    public static String myCustomFieldAction(Action action, String input) {
+    @AtlasActionProcessor
+    public static String myCustomFieldAction(MyFieldActionsModel action, String input) {
         return "Hello " + input;
     }
 
