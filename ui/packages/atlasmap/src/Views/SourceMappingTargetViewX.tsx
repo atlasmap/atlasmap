0b1886a3b581f20a224fae9df0b501e93809ff00
John Verhaeg <verhaeg@icloud.com>
2020-06-11 04:14:43 -0500

    Initial storyboard for transformation approach

---
+++ ui/packages/atlasmap/src/Views/SourceMappingTargetViewX.tsx
@@ -0,0 +1,96 @@
+import React, { FunctionComponent, useState } from "react";
+
+import { Column, ColumnMapper, Columns, NodeRefProvider } from "../UI";
+import {
+  IMappingDocumentEvents,
+  IMappingsColumnData,
+  ISourceColumnCallbacks,
+  ISourceMappingTargetLinksData,
+  ISourceMappingTargetLinksEvents,
+  ISourcesColumnData,
+  ITargetsColumnData,
+  ITargetsColumnCallbacks,
+  SourceMappingTargetLinks,
+  SourcesColumn,
+  TargetsColumn,
+} from "./ColumnMapperView";
+import { IAtlasmapMapping } from "./models";
+import { MappingsColumnX } from "./ColumnMapperView/Columns/MappingsColumnX";
+
+export interface ISourceMappingTargetViewProps
+  extends ISourcesColumnData,
+    IMappingsColumnData,
+    ITargetsColumnData,
+    ISourceMappingTargetLinksData {
+  onSelectMapping: ISourceMappingTargetLinksEvents["onSelectMapping"];
+  sourceEvents: ISourceColumnCallbacks;
+  mappingEvents: IMappingDocumentEvents;
+  targetEvents: ITargetsColumnCallbacks;
+}
+
+export const SourceMappingTargetViewX: FunctionComponent<ISourceMappingTargetViewProps> = ({
+  properties,
+  constants,
+  sources,
+  mappings,
+  targets,
+  showMappingPreview,
+  showTypes,
+  selectedMappingId,
+  onSelectMapping,
+  sourceEvents,
+  mappingEvents,
+  targetEvents,
+}) => {
+  const [highlightedMappingId, setHighlightedMappingId] = useState<
+    string | undefined
+  >();
+  const handleMouseOver = (m: IAtlasmapMapping) =>
+    setHighlightedMappingId(m.id);
+  const handleMouseOut = () => setHighlightedMappingId(undefined);
+  console.log("selectedMappingId: ", selectedMappingId);
+
+  return (
+    <ColumnMapper role={"main"}>
+      <NodeRefProvider>
+        <Columns>
+          <Column data-testid={"column-source-area"} totalColumns={3}>
+            <SourcesColumn
+              properties={properties}
+              constants={constants}
+              sources={sources}
+              showTypes={showTypes}
+              {...sourceEvents}
+            />
+          </Column>
+          <Column data-testid={"column-mappings-area"} totalColumns={3}>
+            <MappingsColumnX
+              mappings={mappings}
+              selectedMappingId={selectedMappingId}
+              showMappingPreview={showMappingPreview}
+              onMouseOver={handleMouseOver}
+              onMouseOut={handleMouseOut}
+              {...mappingEvents}
+            />
+          </Column>
+          <Column data-testid={"column-target-area"} totalColumns={3}>
+            <TargetsColumn
+              showMappingPreview={showMappingPreview}
+              showTypes={showTypes}
+              targets={targets}
+              {...targetEvents}
+            />
+          </Column>
+        </Columns>
+        <SourceMappingTargetLinks
+          mappings={mappings}
+          selectedMappingId={selectedMappingId}
+          highlightedMappingId={highlightedMappingId}
+          onSelectMapping={onSelectMapping}
+          onMouseOver={handleMouseOver}
+          onMouseOut={handleMouseOut}
+        />
+      </NodeRefProvider>
+    </ColumnMapper>
+  );
+};
