6548a44bd01631c1ec983b9bd3e01ec1a3d0243b
pleacu <pleacu@redhat.com>
2021-02-02 13:18:41 -0500

    fix: Corrects d&d on Chrome. Fixes: #2307

--- ui/packages/atlasmap/src/UI/dnd/FieldsDndProvider.tsx
+++ ui/packages/atlasmap/src/UI/dnd/FieldsDndProvider.tsx
@@ -11,22 +11,17 @@ import { IDragAndDropField } from "./models";
 
 const probablyTouch =
   window.matchMedia &&
-  window.matchMedia("(pointer: none)").matches &&
+  (window.matchMedia("(pointer: none)").matches ||
+    window.matchMedia("(pointer: coarse)").matches) &&
   window.matchMedia("(hover: none)").matches;
-const maybeTouch =
-  window.matchMedia &&
-  (window.matchMedia("(pointer: coarse)").matches ||
-    window.matchMedia("(hover: none)").matches); // iOS is triggering this check, probably because it supports the Apple Pencil
 
-const TouchOnlyProvider: FunctionComponent = ({ children }) => (
-  <DndProvider backend={TouchBackend}>{children}</DndProvider>
-);
-
-const TouchAndPointerProvider: FunctionComponent = ({ children }) => (
-  <DndProvider backend={TouchBackend} options={{ enableMouseEvents: true }}>
-    {children}
-  </DndProvider>
-);
+const TouchAndPointerProvider: FunctionComponent = ({ children }) => {
+  return (
+    <DndProvider backend={TouchBackend} options={{ enableMouseEvents: true }}>
+      {children}
+    </DndProvider>
+  );
+};
 
 const MouseOnlyProvider: FunctionComponent = ({ children }) => (
   <DndProvider backend={Html5Backend}>{children}</DndProvider>
@@ -44,11 +39,7 @@ export const FieldsDndProvider: FunctionComponent = ({ children }) => {
   const getHoveredTarget = () => hoveredTarget.current;
   const setHoveredTarget = (target: IDragAndDropField | null) =>
     (hoveredTarget.current = target);
-  const Provider = probablyTouch
-    ? TouchOnlyProvider
-    : maybeTouch
-    ? TouchAndPointerProvider
-    : MouseOnlyProvider;
+  const Provider = probablyTouch ? TouchAndPointerProvider : MouseOnlyProvider;
   return (
     <Provider>
       <FieldsDndContext.Provider value={{ getHoveredTarget, setHoveredTarget }}>
