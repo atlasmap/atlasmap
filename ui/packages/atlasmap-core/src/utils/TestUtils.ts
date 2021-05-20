9cd8b68631f22de7198080fe6c85f403ed25f72f
Tomohisa Igarashi <tm.igarashi@gmail.com>
2021-05-20 09:48:23 -0400

    chore: Applied license header for UI

--- ui/packages/atlasmap-core/src/utils/TestUtils.ts
+++ ui/packages/atlasmap-core/src/utils/TestUtils.ts
@@ -1,5 +1,5 @@
 /*
-    Copyright (C) 2021 Red Hat, Inc.
+    Copyright (C) 2017 Red Hat, Inc.
 
     Licensed under the Apache License, Version 2.0 (the "License");
     you may not use this file except in compliance with the License.
@@ -12,7 +12,8 @@
     WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
     See the License for the specific language governing permissions and
     limitations under the License.
-
+*/
+/*
     Deep diff of JSON objects.  Return true if they match, false otherwise.
 */
 export class TestUtils {
