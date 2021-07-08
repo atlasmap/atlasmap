7e7556b1347b8cbf8a9661d975549a2dabd8cde5
Tomohisa Igarashi <tm.igarashi@gmail.com>
2021-07-08 16:30:34 -0400

    fix: "User defined" separator not working
    Fixes: #2424

--- ui/packages/atlasmap/src/UI/MappingTransformation.stories.tsx
+++ ui/packages/atlasmap/src/UI/MappingTransformation.stories.tsx
@@ -1,46 +1,61 @@
-import React from "react";
+/*
+    Copyright (C) 2017 Red Hat, Inc.
 
-import { boolean } from "@storybook/addon-knobs";
+    Licensed under the Apache License, Version 2.0 (the "License");
+    you may not use this file except in compliance with the License.
+    You may obtain a copy of the License at
+
+            http://www.apache.org/licenses/LICENSE-2.0
+
+    Unless required by applicable law or agreed to in writing, software
+    distributed under the License is distributed on an "AS IS" BASIS,
+    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+    See the License for the specific language governing permissions and
+    limitations under the License.
+*/
+import {
+  ITransformationArgument,
+  MappingTransformation,
+} from "./MappingTransformation";
+import { boolean, text } from "@storybook/addon-knobs";
+import { Form } from "@patternfly/react-core";
+import React from "react";
+import { TransitionModel } from "@atlasmap/core";
 import { action } from "@storybook/addon-actions";
 
-import { MappingTransformation } from "./MappingTransformation";
-import { Form } from "@patternfly/react-core";
-
-export default {
+const obj = {
   title: "UI|Mapping Details/Transformation",
 };
+export default obj;
+
+TransitionModel.initialize();
 
 const transformationsOptions = [
   { name: "Transformation foo", value: "foo" },
   { name: "Transformation bar", value: "bar" },
 ];
 
-const transformationsArguments = [
+const transformationsArguments: ITransformationArgument[] = [
   {
     label: "Argument foo",
     name: "foo",
-    value: "foo",
+    value: text("Argument foo value", "foo"),
   },
   {
     label: "Argument bar",
     name: "bar",
-    value: "bar",
+    value: text("Argument bar value", "bar"),
   },
   {
     label: "Delimiter",
-    name: "baz",
-    value: "baz",
-    delimitersOptions: [
-      {
-        displayName: ",",
-        delimiterValue: ",",
-      },
-      {
-        displayName: "|",
-        delimiterValue: "|",
-      },
-    ],
-    delimiter: ",",
+    name: "Delimiter",
+    value: text("Delimiter value", "baz"),
+    options: TransitionModel.delimiterModels.map((model) => {
+      return {
+        name: model.prettyName ? model.prettyName : "",
+        value: model.actualDelimiter,
+      };
+    }),
   },
 ];
 
