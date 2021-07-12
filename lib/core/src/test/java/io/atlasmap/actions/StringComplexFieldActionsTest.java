151dc6516b4427aa9bdc200d051e839a378bc74b
Tomohisa Igarashi <tm.igarashi@gmail.com>
2021-07-12 11:11:51 -0400

    fix: Split: Some characters doesn't work as a delimiter
    Fixes: #3065

--- lib/core/src/test/java/io/atlasmap/actions/StringComplexFieldActionsTest.java
+++ lib/core/src/test/java/io/atlasmap/actions/StringComplexFieldActionsTest.java
@@ -380,6 +380,23 @@ public class StringComplexFieldActionsTest {
     }
 
     @Test
+    public void testSplitEscape() {
+        Split action = new Split();
+        action.setDelimiter("\\");
+        assertArrayEquals(null, StringComplexFieldActions.split(action, null));
+        assertArrayEquals(new String[] {"1", "2", "3"}, StringComplexFieldActions.split(action, "1\\2\\3"));
+        action.setDelimiter("|");
+        assertArrayEquals(null, StringComplexFieldActions.split(action, null));
+        assertArrayEquals(new String[] {"1", "2", "3"}, StringComplexFieldActions.split(action, "1|2|3"));
+        action.setDelimiter(".");
+        assertArrayEquals(null, StringComplexFieldActions.split(action, null));
+        assertArrayEquals(new String[] {"1", "2", "3"}, StringComplexFieldActions.split(action, "1.2.3"));
+        action.setDelimiter("$");
+        assertArrayEquals(null, StringComplexFieldActions.split(action, null));
+        assertArrayEquals(new String[] {"1", "2", "3"}, StringComplexFieldActions.split(action, "1$2$3"));
+    }
+
+    @Test
     public void testStartsWith() {
         StartsWith action = new StartsWith();
         action.setString("");
