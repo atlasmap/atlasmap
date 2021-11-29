acf3a4c55ef48a12ed05e473d1341dec44b594d2
Tomohisa Igarashi <tm.igarashi@gmail.com>
2021-11-29 09:03:08 -0500

    chore: Update to prettier 2.5.0
    Fixes: #3532

--- ui/packages/atlasmap/src/UI/ExpressionFieldSearch.tsx
+++ ui/packages/atlasmap/src/UI/ExpressionFieldSearch.tsx
@@ -30,149 +30,150 @@ export interface IExpressionFieldSearchProps {
 }
 let selectedField = '';
 
-export const ExpressionFieldSearch: FunctionComponent<IExpressionFieldSearchProps> =
-  ({ clearSearchMode, insertSelectedField, mappedFieldCandidates }) => {
-    function onToggleFieldSearch(toggled: boolean): any {
-      if (!toggled) {
-        mappedFieldCandidates = [];
-        clearSearchMode(true);
-        candidateSrcElement = null;
-      }
+export const ExpressionFieldSearch: FunctionComponent<
+  IExpressionFieldSearchProps
+> = ({ clearSearchMode, insertSelectedField, mappedFieldCandidates }) => {
+  function onToggleFieldSearch(toggled: boolean): any {
+    if (!toggled) {
+      mappedFieldCandidates = [];
+      clearSearchMode(true);
+      candidateSrcElement = null;
     }
+  }
 
-    const id = `expr-field-search-${selectedField}`;
-    let candidateSrcElement: any;
-    const { state, toggle, toggleOff } = useToggle(true, onToggleFieldSearch);
+  const id = `expr-field-search-${selectedField}`;
+  let candidateSrcElement: any;
+  const { state, toggle, toggleOff } = useToggle(true, onToggleFieldSearch);
 
-    /**
-     * Track a candidate selection from either a mouse hover or arrow
-     * key navigation.
-     *
-     * @param event
-     * @param index
-     */
-    function trackSelection(event: any): void {
-      if (event.srcElement) {
-        // const docId = event.target.getAttribute('label');
-        candidateSrcElement = event.srcElement;
-      }
+  /**
+   * Track a candidate selection from either a mouse hover or arrow
+   * key navigation.
+   *
+   * @param event
+   * @param index
+   */
+  function trackSelection(event: any): void {
+    if (event.srcElement) {
+      // const docId = event.target.getAttribute('label');
+      candidateSrcElement = event.srcElement;
     }
+  }
 
-    /**
-     * Update the candidate source element and reset the focus.
-     *
-     * @param sibling
-     */
-    function updateCandidate(sibling: any): void {
-      if (candidateSrcElement && sibling) {
-        candidateSrcElement.style.backgroundColor = 'white';
-        sibling.focus();
-        candidateSrcElement = sibling;
+  /**
+   * Update the candidate source element and reset the focus.
+   *
+   * @param sibling
+   */
+  function updateCandidate(sibling: any): void {
+    if (candidateSrcElement && sibling) {
+      candidateSrcElement.style.backgroundColor = 'white';
+      sibling.focus();
+      candidateSrcElement = sibling;
+      candidateSrcElement.style.backgroundColor = 'lightblue';
+    }
+  }
+
+  function onKeyHandler(
+    _index: number,
+    _innerIndex: number,
+    _position: string,
+  ): void {
+    // TODO
+  }
+
+  function onKeyDown(event: any): void {
+    if ('Enter' === event.key) {
+      event.preventDefault();
+      if (candidateSrcElement) {
+        // insertSelectedField(candidateIndex);
+      }
+    } else if ('ArrowDown' === event.key) {
+      event.preventDefault();
+      trackSelection(event);
+      if (candidateSrcElement) {
+        updateCandidate(candidateSrcElement.nextElementSibling);
+      }
+    } else if ('ArrowUp' === event.key) {
+      event.preventDefault();
+      trackSelection(event);
+      if (candidateSrcElement) {
+        updateCandidate(candidateSrcElement.previousElementSibling);
+      }
+    } else if ('Tab' === event.key) {
+      if (!candidateSrcElement && event.srcElement) {
+        candidateSrcElement =
+          event.srcElement.nextElementSibling.firstElementChild;
         candidateSrcElement.style.backgroundColor = 'lightblue';
-      }
-    }
-
-    function onKeyHandler(
-      _index: number,
-      _innerIndex: number,
-      _position: string,
-    ): void {
-      // TODO
-    }
-
-    function onKeyDown(event: any): void {
-      if ('Enter' === event.key) {
+      } else if (
+        candidateSrcElement &&
+        candidateSrcElement.nextElementSibling
+      ) {
         event.preventDefault();
-        if (candidateSrcElement) {
-          // insertSelectedField(candidateIndex);
-        }
-      } else if ('ArrowDown' === event.key) {
-        event.preventDefault();
-        trackSelection(event);
-        if (candidateSrcElement) {
-          updateCandidate(candidateSrcElement.nextElementSibling);
-        }
-      } else if ('ArrowUp' === event.key) {
-        event.preventDefault();
-        trackSelection(event);
-        if (candidateSrcElement) {
-          updateCandidate(candidateSrcElement.previousElementSibling);
-        }
-      } else if ('Tab' === event.key) {
-        if (!candidateSrcElement && event.srcElement) {
-          candidateSrcElement =
-            event.srcElement.nextElementSibling.firstElementChild;
-          candidateSrcElement.style.backgroundColor = 'lightblue';
-        } else if (
-          candidateSrcElement &&
-          candidateSrcElement.nextElementSibling
-        ) {
-          event.preventDefault();
-          updateCandidate(candidateSrcElement!.nextElementSibling);
-        }
+        updateCandidate(candidateSrcElement!.nextElementSibling);
       }
     }
+  }
 
-    function selectionChanged(
-      event: any,
-      value: string | SelectOptionObject,
-      isPlaceholder?: boolean | undefined,
-    ): void {
-      if (!isPlaceholder) {
-        const docId = event.target.getAttribute('label');
-        selectedField = value as string;
-        insertSelectedField(docId, selectedField);
-      }
-      onToggleFieldSearch(false);
-      toggleOff();
+  function selectionChanged(
+    event: any,
+    value: string | SelectOptionObject,
+    isPlaceholder?: boolean | undefined,
+  ): void {
+    if (!isPlaceholder) {
+      const docId = event.target.getAttribute('label');
+      selectedField = value as string;
+      insertSelectedField(docId, selectedField);
     }
+    onToggleFieldSearch(false);
+    toggleOff();
+  }
 
-    function createSelectOption(selectField: string[], idx: number): any {
-      // Use the display name for documents and field path for fields.
-      if (selectField[1][0] !== '/') {
-        return (
-          <SelectOption
-            label={selectField[0]}
-            value={selectField[0]}
-            key={idx}
-            index={idx}
-            className={styles.document}
-            isPlaceholder={true}
-          />
-        );
-      } else {
-        return (
-          <SelectOption
-            label={selectField[0]}
-            value={selectField[1]}
-            key={idx}
-            keyHandler={onKeyHandler}
-            index={idx}
-            className={styles.field}
-          />
-        );
-      }
+  function createSelectOption(selectField: string[], idx: number): any {
+    // Use the display name for documents and field path for fields.
+    if (selectField[1][0] !== '/') {
+      return (
+        <SelectOption
+          label={selectField[0]}
+          value={selectField[0]}
+          key={idx}
+          index={idx}
+          className={styles.document}
+          isPlaceholder={true}
+        />
+      );
+    } else {
+      return (
+        <SelectOption
+          label={selectField[0]}
+          value={selectField[1]}
+          key={idx}
+          keyHandler={onKeyHandler}
+          index={idx}
+          className={styles.field}
+        />
+      );
     }
+  }
 
-    return (
-      <div
-        aria-label="Expression Field Search"
-        className={styles.searchMenu}
-        data-testid={'expression-field-search'}
+  return (
+    <div
+      aria-label="Expression Field Search"
+      className={styles.searchMenu}
+      data-testid={'expression-field-search'}
+    >
+      <Select
+        onToggle={toggle}
+        isOpen={state}
+        value={selectedField}
+        id={id}
+        onKeyDown={onKeyDown}
+        onSelect={selectionChanged}
+        data-testid={id}
       >
-        <Select
-          onToggle={toggle}
-          isOpen={state}
-          value={selectedField}
-          id={id}
-          onKeyDown={onKeyDown}
-          onSelect={selectionChanged}
-          data-testid={id}
-        >
-          {mappedFieldCandidates.map((s, idx: number) =>
-            createSelectOption(s, idx),
-          )}
-        </Select>
-      </div>
-    );
-  };
+        {mappedFieldCandidates.map((s, idx: number) =>
+          createSelectOption(s, idx),
+        )}
+      </Select>
+    </div>
+  );
+};
--- ui/packages/atlasmap/src/UI/MappingTransformation.tsx
+++ ui/packages/atlasmap/src/UI/MappingTransformation.tsx
@@ -75,230 +75,231 @@ const formTransTextInputStyle = {
   marginTop: '0rem',
 } as React.CSSProperties;
 
-export const MappingTransformation: FunctionComponent<IMappingTransformationProps> =
-  ({
-    name,
-    transformationsOptions,
-    transformationsArguments = [],
-    disableTransformation,
-    onTransformationArgumentChange,
-    onTransformationChange,
-    onRemoveTransformation,
-  }) => {
-    const RenderTransformationArgumentOptions = (
-      argId: string,
-      arg: ITransformationArgument,
-      disableTransformation: boolean,
-      onTransformationArgumentChange: (name: string, value: string) => void,
+export const MappingTransformation: FunctionComponent<
+  IMappingTransformationProps
+> = ({
+  name,
+  transformationsOptions,
+  transformationsArguments = [],
+  disableTransformation,
+  onTransformationArgumentChange,
+  onTransformationChange,
+  onRemoveTransformation,
+}) => {
+  const RenderTransformationArgumentOptions = (
+    argId: string,
+    arg: ITransformationArgument,
+    disableTransformation: boolean,
+    onTransformationArgumentChange: (name: string, value: string) => void,
+  ) => {
+    /** If the option name is 'User defined', we assume it accepts a user input */
+    const isUserDefined = (option?: ITransformationSelectOption) => {
+      return option?.name === 'User defined';
+    };
+
+    const userDefinedOption = arg.options?.find((option) =>
+      isUserDefined(option),
+    );
+
+    let initialSelected = arg.options?.find(
+      (option) => option.value === arg.value,
+    );
+    if (!initialSelected) {
+      if (arg.value) {
+        initialSelected = userDefinedOption;
+      } else if (arg.options) {
+        initialSelected = arg.options[1];
+        arg.value = arg.options[1].value;
+      }
+    }
+    let initialUserDefined = '';
+    if (isUserDefined(initialSelected)) {
+      initialUserDefined = arg.value;
+    }
+
+    const [isOpen, setIsOpen] = useState(false);
+    const [selected, setSelected] = useState<
+      ITransformationSelectOption | undefined
+    >(initialSelected);
+    const [userDefinedValue, setUserDefinedValue] =
+      useState<string>(initialUserDefined);
+
+    const onToggle = (isOpen: boolean) => {
+      setIsOpen(isOpen);
+    };
+
+    const onSelect = (
+      _event:
+        | React.MouseEvent<Element, MouseEvent>
+        | React.ChangeEvent<Element>,
+      selection: string | SelectOptionObject,
+      _isPlaceholder?: boolean,
     ) => {
-      /** If the option name is 'User defined', we assume it accepts a user input */
-      const isUserDefined = (option?: ITransformationSelectOption) => {
-        return option?.name === 'User defined';
-      };
-
-      const userDefinedOption = arg.options?.find((option) =>
-        isUserDefined(option),
+      setIsOpen(false);
+      const selectedOption = arg.options!.find(
+        (option) => option.value === selection,
       );
-
-      let initialSelected = arg.options?.find(
-        (option) => option.value === arg.value,
-      );
-      if (!initialSelected) {
-        if (arg.value) {
-          initialSelected = userDefinedOption;
-        } else if (arg.options) {
-          initialSelected = arg.options[1];
-          arg.value = arg.options[1].value;
-        }
+      if (!selectedOption) {
+        return;
       }
-      let initialUserDefined = '';
-      if (isUserDefined(initialSelected)) {
-        initialUserDefined = arg.value;
-      }
-
-      const [isOpen, setIsOpen] = useState(false);
-      const [selected, setSelected] = useState<
-        ITransformationSelectOption | undefined
-      >(initialSelected);
-      const [userDefinedValue, setUserDefinedValue] =
-        useState<string>(initialUserDefined);
-
-      const onToggle = (isOpen: boolean) => {
-        setIsOpen(isOpen);
-      };
-
-      const onSelect = (
-        _event:
-          | React.MouseEvent<Element, MouseEvent>
-          | React.ChangeEvent<Element>,
-        selection: string | SelectOptionObject,
-        _isPlaceholder?: boolean,
-      ) => {
-        setIsOpen(false);
-        const selectedOption = arg.options!.find(
-          (option) => option.value === selection,
-        );
-        if (!selectedOption) {
-          return;
-        }
-        setSelected(selectedOption);
-        arg.value = isUserDefined(selectedOption)
+      setSelected(selectedOption);
+      arg.value = isUserDefined(selectedOption)
+        ? userDefinedValue
           ? userDefinedValue
-            ? userDefinedValue
-            : ''
-          : selection.toString();
-        onTransformationArgumentChange(arg.name, arg.value);
-      };
-
-      const onChangeUserDefinedValue = (value: string) => {
-        setUserDefinedValue(value);
-        arg.value = value;
-        onTransformationArgumentChange(arg.name, value);
-      };
-
-      return (
-        <Split>
-          <SplitItem>
-            <Select
-              variant={SelectVariant.single}
-              aria-label={arg.label}
-              onSelect={onSelect}
-              selections={selected?.value}
-              onToggle={onToggle}
-              isOpen={isOpen}
-              placeholderText={'[None]'}
-              id={argId}
-              isDisabled={disableTransformation}
-              data-testid={arg.name}
-              style={formTransStyle}
-            >
-              {arg.options?.map((option, optIndx) => {
-                return (
-                  <SelectOption value={option.value} key={optIndx}>
-                    {option.name}
-                  </SelectOption>
-                );
-              })}
-            </Select>
-          </SplitItem>
-          {isUserDefined(selected) && (
-            <SplitItem>
-              <TextInput
-                id="userDefined"
-                type="text"
-                name="userDefined"
-                value={userDefinedValue}
-                onChange={onChangeUserDefinedValue}
-                style={formTransTextInputStyle}
-                data-testid={`userDefined`}
-                autoFocus
-              />
-            </SplitItem>
-          )}
-        </Split>
-      );
+          : ''
+        : selection.toString();
+      onTransformationArgumentChange(arg.name, arg.value);
     };
 
-    const renderTransformationArgumentText = (
-      argId: string,
-      a: ITransformationArgument,
-    ) => {
-      return (
-        <TextInput
-          id={argId}
-          type="text"
-          name={a.name}
-          isDisabled={disableTransformation}
-          value={a.value}
-          onChange={(value) => onTransformationArgumentChange(a.name, value)}
-          data-testid={`insert-transformation-parameter-${a.name}-input-field`}
-          style={formTransTextInputStyle}
-        />
-      );
+    const onChangeUserDefinedValue = (value: string) => {
+      setUserDefinedValue(value);
+      arg.value = value;
+      onTransformationArgumentChange(arg.name, value);
     };
 
-    const renderTransformationArgumentBoolean = (
-      argId: string,
-      a: ITransformationArgument,
-    ) => {
-      return (
-        <Checkbox
-          className={css(styles.transArgs)}
-          id={argId}
-          data-testid={argId + '-checkbox'}
-          key={argId}
-          label={a.label}
-          aria-label={a.label}
-          isChecked={a.value === 'true'}
-          onChange={(value) =>
-            onTransformationArgumentChange(a.name, value.toString())
-          }
-        />
-      );
-    };
-
-    const renderTransformationArgument = (
-      a: ITransformationArgument,
-      idx: number,
-    ) => {
-      const argId = `${id}-transformation-${idx}`;
-      switch (a.type) {
-        case FieldType.BOOLEAN: {
-          return renderTransformationArgumentBoolean(argId, a);
-        }
-        default: {
-          return (
-            <FormGroup
-              className={css(styles.transArgs)}
-              fieldId={argId}
-              label={a.label}
-              key={idx}
-              style={formTransGroupStyle}
-            >
-              {a.options
-                ? RenderTransformationArgumentOptions(
-                    argId,
-                    a,
-                    disableTransformation,
-                    onTransformationArgumentChange,
-                  )
-                : renderTransformationArgumentText(argId, a)}
-            </FormGroup>
-          );
-        }
-      }
-    };
-
-    const id = `user-field-action-${name}`;
     return (
-      <>
-        <FormGroup fieldId={`${id}-transformation`}>
-          <InputGroup style={{ background: 'transparent' }}>
-            <FormSelect
-              value={name}
-              id={id}
-              isDisabled={disableTransformation}
-              onChange={onTransformationChange}
-              data-testid={id}
-              style={formTransStyle}
-            >
-              {transformationsOptions.map((a, idx) => (
-                <FormSelectOption label={a.name} value={a.value} key={idx} />
-              ))}
-            </FormSelect>
-            {onRemoveTransformation && (
-              <Button
-                variant={'plain'}
-                onClick={onRemoveTransformation}
-                data-testid={`close-transformation-${name}-button`}
-                aria-label="Remove the transformation"
-              >
-                <TrashIcon />
-              </Button>
-            )}
-          </InputGroup>
-        </FormGroup>
-        {transformationsArguments.map(renderTransformationArgument)}
-      </>
+      <Split>
+        <SplitItem>
+          <Select
+            variant={SelectVariant.single}
+            aria-label={arg.label}
+            onSelect={onSelect}
+            selections={selected?.value}
+            onToggle={onToggle}
+            isOpen={isOpen}
+            placeholderText={'[None]'}
+            id={argId}
+            isDisabled={disableTransformation}
+            data-testid={arg.name}
+            style={formTransStyle}
+          >
+            {arg.options?.map((option, optIndx) => {
+              return (
+                <SelectOption value={option.value} key={optIndx}>
+                  {option.name}
+                </SelectOption>
+              );
+            })}
+          </Select>
+        </SplitItem>
+        {isUserDefined(selected) && (
+          <SplitItem>
+            <TextInput
+              id="userDefined"
+              type="text"
+              name="userDefined"
+              value={userDefinedValue}
+              onChange={onChangeUserDefinedValue}
+              style={formTransTextInputStyle}
+              data-testid={`userDefined`}
+              autoFocus
+            />
+          </SplitItem>
+        )}
+      </Split>
     );
   };
+
+  const renderTransformationArgumentText = (
+    argId: string,
+    a: ITransformationArgument,
+  ) => {
+    return (
+      <TextInput
+        id={argId}
+        type="text"
+        name={a.name}
+        isDisabled={disableTransformation}
+        value={a.value}
+        onChange={(value) => onTransformationArgumentChange(a.name, value)}
+        data-testid={`insert-transformation-parameter-${a.name}-input-field`}
+        style={formTransTextInputStyle}
+      />
+    );
+  };
+
+  const renderTransformationArgumentBoolean = (
+    argId: string,
+    a: ITransformationArgument,
+  ) => {
+    return (
+      <Checkbox
+        className={css(styles.transArgs)}
+        id={argId}
+        data-testid={argId + '-checkbox'}
+        key={argId}
+        label={a.label}
+        aria-label={a.label}
+        isChecked={a.value === 'true'}
+        onChange={(value) =>
+          onTransformationArgumentChange(a.name, value.toString())
+        }
+      />
+    );
+  };
+
+  const renderTransformationArgument = (
+    a: ITransformationArgument,
+    idx: number,
+  ) => {
+    const argId = `${id}-transformation-${idx}`;
+    switch (a.type) {
+      case FieldType.BOOLEAN: {
+        return renderTransformationArgumentBoolean(argId, a);
+      }
+      default: {
+        return (
+          <FormGroup
+            className={css(styles.transArgs)}
+            fieldId={argId}
+            label={a.label}
+            key={idx}
+            style={formTransGroupStyle}
+          >
+            {a.options
+              ? RenderTransformationArgumentOptions(
+                  argId,
+                  a,
+                  disableTransformation,
+                  onTransformationArgumentChange,
+                )
+              : renderTransformationArgumentText(argId, a)}
+          </FormGroup>
+        );
+      }
+    }
+  };
+
+  const id = `user-field-action-${name}`;
+  return (
+    <>
+      <FormGroup fieldId={`${id}-transformation`}>
+        <InputGroup style={{ background: 'transparent' }}>
+          <FormSelect
+            value={name}
+            id={id}
+            isDisabled={disableTransformation}
+            onChange={onTransformationChange}
+            data-testid={id}
+            style={formTransStyle}
+          >
+            {transformationsOptions.map((a, idx) => (
+              <FormSelectOption label={a.name} value={a.value} key={idx} />
+            ))}
+          </FormSelect>
+          {onRemoveTransformation && (
+            <Button
+              variant={'plain'}
+              onClick={onRemoveTransformation}
+              data-testid={`close-transformation-${name}-button`}
+              aria-label="Remove the transformation"
+            >
+              <TrashIcon />
+            </Button>
+          )}
+        </InputGroup>
+      </FormGroup>
+      {transformationsArguments.map(renderTransformationArgument)}
+    </>
+  );
+};
