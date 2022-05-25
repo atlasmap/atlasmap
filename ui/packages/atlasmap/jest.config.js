cbecb729fae769c8b77063555c11c59966293002
kahboom <rachelyordan@gmail.com>
2022-05-25 11:33:50 +0100

    add separate babel config

--- ui/packages/atlasmap/jest.config.js
+++ ui/packages/atlasmap/jest.config.js
@@ -2,10 +2,12 @@ const path = require('path');
 module.exports = {
   globals: {
     'ts-jest': {
+      allowJs: true,
       isolatedModules: true,
       tsConfig: `tsconfig.test.json`,
     },
   },
+  moduleDirectories: ["node_modules", "src"],
   moduleNameMapper: {
     '\\.(css|less)$': path.resolve(__dirname, './test/__mocks__/styleMock.js'),
     '\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$': path.resolve(
@@ -20,11 +22,14 @@ module.exports = {
   setupFilesAfterEnv: [path.resolve(__dirname, './test/setup.ts')],
   testEnvironment: 'jest-environment-jsdom-sixteen',
   transform: {
+    '^.+\\.(js|jsx)$': 'babel-jest',
     '^.+\\.(ts|tsx)$': 'ts-jest',
     '^.+\\.(css)$': 'jest-css-modules-transform'
   },
   transformIgnorePatterns: [
+    '(?!atlasmap-core)',
     '/node_modules/(?!ky)',
     '/node_modules/(?!monaco-editor)'
-  ]
+  ],
+  verbose: true,
 };
