e044960e2deb15abee73cd383a9ea104f6463afe
kahboom <rachelyordan@gmail.com>
2022-05-25 09:25:15 +0100

    move jest config to dedicated file, add babel for es module support for jest

---
+++ ui/packages/atlasmap/jest.config.ts
@@ -0,0 +1,30 @@
+const path = require('path');
+module.exports = {
+  globals: {
+    'ts-jest': {
+      isolatedModules: true,
+      tsConfig: `tsconfig.test.json`,
+    },
+  },
+  moduleNameMapper: {
+    '\\.(css|less)$': path.resolve(__dirname, './test/__mocks__/styleMock.js'),
+    '\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$': path.resolve(
+      __dirname,
+      './test/__mocks__/fileMock.js'
+    ),
+    'ky': 'ky/umd',
+    'monaco-editor': 'monaco-editor/esm/vs/editor/editor.api.js',
+    '@atlasmap/core': path.resolve(__dirname, '../atlasmap-core/src')
+  },
+  preset: 'ts-jest',
+  setupFilesAfterEnv: [path.resolve(__dirname, './test/setup.ts')],
+  testEnvironment: 'jest-environment-jsdom-sixteen',
+  transform: {
+    '^.+\\.(ts|tsx)$': 'ts-jest',
+    '^.+\\.(css)$': 'jest-css-modules-transform'
+  },
+  transformIgnorePatterns: [
+    '/node_modules/(?!ky)',
+    '/node_modules/(?!monaco-editor)'
+  ]
+};
