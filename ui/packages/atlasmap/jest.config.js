ac15216ae9d08ae96ba5988aeadf0e68ebb2df10
kahboom <rachelyordan@gmail.com>
2022-05-31 18:02:43 +0100

    handle css modules with identity-obj-proxy

--- ui/packages/atlasmap/jest.config.js
+++ ui/packages/atlasmap/jest.config.js
@@ -8,7 +8,8 @@ module.exports = {
     },
   },
   moduleNameMapper: {
-    '\\.(css|less)$': path.resolve(__dirname, './test/__mocks__/styleMock.js'),
+    // '\\.(css|less)$': path.resolve(__dirname, './test/__mocks__/styleMock.js'),
+    '\\.(css|less|sass|scss)$': 'identity-obj-proxy',
     '\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$': path.resolve(
       __dirname,
       './test/__mocks__/fileMock.js'
