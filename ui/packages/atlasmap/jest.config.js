2abcdeda35e43c0a016c25a92963473575bb838a
kahboom <rachelyordan@gmail.com>
2022-05-31 16:35:32 +0100

    fix for tests

--- ui/packages/atlasmap/jest.config.js
+++ ui/packages/atlasmap/jest.config.js
@@ -7,7 +7,6 @@ module.exports = {
       tsConfig: `tsconfig.test.json`,
     },
   },
-  moduleDirectories: ["node_modules", "src"],
   moduleNameMapper: {
     '\\.(css|less)$': path.resolve(__dirname, './test/__mocks__/styleMock.js'),
     '\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$': path.resolve(
@@ -15,21 +14,18 @@ module.exports = {
       './test/__mocks__/fileMock.js'
     ),
     'ky': 'ky/umd',
-    'monaco-editor': 'monaco-editor/esm/vs/editor/editor.api.js',
+    'monaco-editor': 'monaco-editor/esm/vs/editor/editor.api.d.ts',
     '@atlasmap/core': path.resolve(__dirname, '../atlasmap-core/src')
   },
   preset: 'ts-jest',
   setupFilesAfterEnv: [path.resolve(__dirname, './test/setup.ts')],
   testEnvironment: 'jest-environment-jsdom-sixteen',
   transform: {
-    '^.+\\.(js|jsx)$': 'babel-jest',
     '^.+\\.(ts|tsx)$': 'ts-jest',
     '^.+\\.(css)$': 'jest-css-modules-transform'
   },
   transformIgnorePatterns: [
-    '(?!atlasmap-core)',
-    '/node_modules/(?!ky)',
-    '/node_modules/(?!monaco-editor)'
+    '/node_modules/(?!(ky|monaco-editor)/)'
   ],
   verbose: true,
 };
