d1c5f1d3c42949a1c5ef05af486cdd233da88906
kahboom <rachelyordan@gmail.com>
2022-05-31 16:39:01 +0100

    cleanup

--- ui/packages/atlasmap-core/jest.config.js
+++ ui/packages/atlasmap-core/jest.config.js
@@ -11,20 +11,20 @@ module.exports = {
       __dirname,
       './test/__mocks__/fileMock.js'
     ),
-    "ky": "ky/umd",
-    "monaco-editor": "monaco-editor/esm/vs/editor/editor.api.js",
-    "@src": "<rootDir>/src/index.ts",
-    "@src/(.*)": "<rootDir>/src/$1",
-    "@test/(.*)": "<rootDir>/test/$1"
+    'ky': 'ky/umd',
+    'monaco-editor': 'monaco-editor/esm/vs/editor/editor.api.js',
+    '@src': '<rootDir>/src/index.ts',
+    '@src/(.*)': '<rootDir>/src/$1',
+    '@test/(.*)': '<rootDir>/test/$1'
   },
   preset: 'ts-jest',
   setupFilesAfterEnv: [path.resolve(__dirname, './test/setup.tsx')],
   testEnvironment: 'jsdom',
   transform: {
-    "monaco-editor": "jest-esm-transformer",
-    "^.+\\.(ts|tsx)$": "ts-jest"
+    'monaco-editor': 'jest-esm-transformer',
+    '^.+\\.(ts|tsx)$': 'ts-jest'
   },
   transformIgnorePatterns: [
-    "/node_modules/(?!(ky|monaco-editor)/)"
+    '/node_modules/(?!(ky|monaco-editor)/)'
   ]
 };
