22b6962c68bf22e7e7bfab4e1a81dfdbe923a1b2
Tomohisa Igarashi <tm.igarashi@gmail.com>
2022-08-15 16:12:40 -0400

    chore: re-format due to lint update

--- ui/packages/atlasmap/src/UI/dialogs/ConfirmationDialog.tsx
+++ ui/packages/atlasmap/src/UI/dialogs/ConfirmationDialog.tsx
@@ -28,47 +28,48 @@ export interface IConfirmationDialogProps {
   isOpen: boolean;
 }
 
-export const ConfirmationDialog: FunctionComponent<IConfirmationDialogProps> =
-  ({
-    title,
-    description,
-    dataTestid,
-    onCancel,
-    onConfirm,
-    isOpen,
-    children,
-  }) => {
-    return (
-      <Modal
-        variant="small"
-        title={title}
-        description={description}
-        data-testid={dataTestid}
-        isOpen={isOpen}
-        onClose={onCancel}
-        actions={[
-          <Button
-            key={'confirm'}
-            variant={'primary'}
-            onClick={onConfirm}
-            aria-label="Confirm"
-            data-testid={'confirmation-dialog-confirm-button'}
-            isDisabled={!onConfirm}
-          >
-            Confirm
-          </Button>,
-          <Button
-            key={'cancel'}
-            variant={'link'}
-            onClick={onCancel}
-            aria-label="Cancel"
-            data-testid={'confirmation-dialog-cancel-button'}
-          >
-            Cancel
-          </Button>,
-        ]}
-      >
-        {children}
-      </Modal>
-    );
-  };
+export const ConfirmationDialog: FunctionComponent<
+  IConfirmationDialogProps
+> = ({
+  title,
+  description,
+  dataTestid,
+  onCancel,
+  onConfirm,
+  isOpen,
+  children,
+}) => {
+  return (
+    <Modal
+      variant="small"
+      title={title}
+      description={description}
+      data-testid={dataTestid}
+      isOpen={isOpen}
+      onClose={onCancel}
+      actions={[
+        <Button
+          key={'confirm'}
+          variant={'primary'}
+          onClick={onConfirm}
+          aria-label="Confirm"
+          data-testid={'confirmation-dialog-confirm-button'}
+          isDisabled={!onConfirm}
+        >
+          Confirm
+        </Button>,
+        <Button
+          key={'cancel'}
+          variant={'link'}
+          onClick={onCancel}
+          aria-label="Cancel"
+          data-testid={'confirmation-dialog-cancel-button'}
+        >
+          Cancel
+        </Button>,
+      ]}
+    >
+      {children}
+    </Modal>
+  );
+};
